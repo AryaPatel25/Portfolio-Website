7788f54acdfcc5de91d212c828008d497b6e947c
Arya Patel <91053876+AryaPatel25@users.noreply.github.com>
2025-05-20 20:53:33 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxtlzk4VweDmkKMTf6mNZcTIhqwpiLNtU",
-  authDomain: "portfolio-website-d19d0.firebaseapp.com",
-  projectId: "portfolio-website-d19d0",
-  storageBucket: "portfolio-website-d19d0.firebasestorage.app",
-  messagingSenderId: "816149486890",
-  appId: "1:816149486890:web:3d898b92748d01741d1253",
-  measurementId: "G-MBG7GHP70Z"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
